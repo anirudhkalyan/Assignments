06c4a5640d89c38372a6a8a0e6dcc82809a92b46
KrishnaYadati <111636216+KrishnaYadati@users.noreply.github.com>
2022-08-23 22:43:02 +0530

    Update main.cpp

--- assignment1/main.cpp
+++ assignment1/main.cpp
@@ -1,34 +1,16 @@
 #include <Arduino.h>
-//Declaring all variables as integers
 int U=1,V=1,W=0;
-int D,C,B,A;
-
-//Code released under GNU GPL.  Free to use for anything.
-void disp_7447( int D, int C, int B, int A)
+int G;
+void setup()
 {
-  digitalWrite(2, A); //LSB
-  digitalWrite(3, B); 
-  digitalWrite(4, C);
-  digitalWrite(5, D);   //MSB
-
+  pinMode(2, OUTPUT);
 }
-// the setup function runs once when you press reset or power the board
-void setup() {
-    pinMode(2, OUTPUT);  
-    pinMode(3, OUTPUT);
-    pinMode(4, OUTPUT);
-    pinMode(5, OUTPUT);
+void loop()
+{
+  G=(U || V || !W) && (U || !V || !W) && (!U || V || !W) && (!U || !V || W);
+  digitalWrite(2, G);
 }
 
-// the loop function runs over and over again forever
-void loop() {
-  A=(U || V || !W) && (U || !V || !W) && (!U || !V || W);
-B=0;
-C=0;
-D=0;
-disp_7447(D,C,B,A);  
-}
 //&& is the AND operation
 // || is the OR operation
 // ! is the NOT operation
-
