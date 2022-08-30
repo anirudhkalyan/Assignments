10e40ea4309057d1f7b2e0c4a84aba74be8e6b38
KrishnaYadati <111636216+KrishnaYadati@users.noreply.github.com>
2022-08-30 16:56:53 +0530

    Add files via upload

---
+++ assignment2/pos_Kmap/pos_kmap.asm
@@ -0,0 +1,46 @@
+.include "/home/krishna/m328Pdef.inc"
+ldi r16,0b00000100  ;2 pin
+out DDRD,r16
+ldi r17, 0b11111000; identifying input pins 8,9,10
+	out DDRB,r17		; declaring pins as input
+ldi r17, 0b11111001;
+	out PORTB,r17		; activating internal pullup for pins 8,9,10 
+	
+ldi r18,0b00000001 ; value
+ldi r19,0b00000001 ; value
+ldi r20,0b00000001; value
+
+;in r17,PINB
+
+and r18,r17 ; W
+lsr r17;
+and r19,r17;V
+lsr r17;
+and r20,r17; U
+lsr r17;
+
+ldi r21,0b00000001
+eor r21,r18;  W'
+ldi r22,0b00000001
+eor r22,r19; V'
+ldi r23,0b00000001
+eor r23,r20; U'
+
+mov r16,r21;W'
+or  r16,r19;V+W'
+mov r24,r21;W'
+or  r24,r20;U+W'
+and r16,r24;(V+W')(U+W')
+mov r25,r18;W
+or r25,r22;V'+W
+or r25,r23;U'+V'+W
+and r16,r25;(V+W')(U+W')(U'+V'+W)
+lsl r16;
+lsl r16;
+
+out PORTD,r16             ;G output
+
+start:
+rjmp start
+
+
